89417c71905648c1f5054c0fdcb0fe5946b9da69
out_fourtime_hugo <rafael@fourtime.com>
2016-12-28 03:22:00 -0200

    Initial commit

---
+++ app/src/main/java/girosoft/com/br/camelodromo/MainActivity.java
@@ -0,0 +1,27 @@
+package girosoft.com.br.camelodromo;
+
+import android.content.Context;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_scrolling);
+
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.MyToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapse_toolbar);
+        collapsingToolbarLayout.setTitle("Camelodromo \n Eldorado");
+
+        Context context = this;
+        collapsingToolbarLayout.setContentScrimColor(ContextCompat.getColor(context,R.color.colorAccent));
+    }
+}
